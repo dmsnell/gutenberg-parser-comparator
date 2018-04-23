d332c65baf82aa4d904e1a0736be08eaafbdcd3c
Dennis Snell <dennis.snell@automattic.com>
2018-04-23 14:55:43 +0100

    add moby dick

--- defaults.js
+++ defaults.js
@@ -13,4 +13,5 @@ https://raw.githubusercontent.com/dmsnell/gutenberg-document-library/master/libr
 https://raw.githubusercontent.com/dmsnell/gutenberg-document-library/master/library/web-at-maximum-fps.html
 https://raw.githubusercontent.com/dmsnell/gutenberg-document-library/master/library/early-adopting-the-future.html
 https://raw.githubusercontent.com/dmsnell/gutenberg-document-library/master/library/pygmalian-raw-html.html
+https://raw.githubusercontent.com/dmsnell/gutenberg-document-library/master/library/moby-dick-parsed.html
 `;
