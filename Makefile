4d7239f73d5c93be642814b6829b780c05ecf304
Dennis Snell <dennis.snell@automattic.com>
2018-04-23 13:31:06 +0100

    add missing files

---
+++ Makefile
@@ -0,0 +1,3 @@
+.PHONY: run
+run:
+	httpserver -p 8001 -H 127.0.0.1
---
+++ index.js
@@ -0,0 +1,237 @@
+// @ts-check 
+const l = m => ( console.log( m ), m );
+
+// how long to iterate when benchmarking
+const LOOP_TIME = 100;
+
+const state = {
+    input: {
+        prevParser: a => false,
+        nextParser: a => false,
+        postList: []
+    },
+
+    posts: {}
+};
+window.state = state;
+
+function initialize( next ) {
+    const prevParser = eval( document.getElementById( 'prev-parser-content' ).value );
+    const nextParser = eval( document.getElementById( 'next-parser-content' ).value );
+    const postList = document.getElementById( 'post-list-content' ).value.split('\n').filter(Boolean);
+
+    state.input.prevParser = prevParser();
+    state.input.nextParser = nextParser();
+    state.input.postList = postList;
+
+    next();
+}
+
+function renderLoadingPosts() {
+    const loadingPosts = document.getElementById( 'loading-posts' );
+
+    let output = '<ul>';
+    for ( let name in state.posts ) {
+        const post = state.posts[ name ];
+        switch ( post.status ) {
+            case 'loading':
+                output += '<li>' + name + ': ' + ( post.transfer.loaded / ( post.transfer.total + Number.EPSILON ) ) + '%</li>';
+                break;
+            
+            case 'loaded':
+                output += '<li>' + name + ': loaded</li>';
+                break;
+            
+            case 'failed':
+                output += '<li>' + name + ': failed (' + post.transfer.status + ')</li>';
+                break;
+        }
+    }
+    output += '</ul>';
+
+    loadingPosts.innerHTML = output;
+}
+
+function loadPosts( next ) {
+    const startedAt = performance.now();
+    const postCount = state.input.postList.length;
+    let loadedPosts = 0;
+
+    const queueNext = () => {
+        renderLoadingPosts();
+
+        if ( loadedPosts === postCount ) {
+            const endedAt = performance.now();
+            document.getElementById( 'loading-posts' ).innerHTML += '<p>Took ' + Math.ceil( endedAt - startedAt ) + ' ms to load posts</p>';
+            next();
+        }
+    }
+
+    for ( let i = 0; i < postCount; i++ ) {
+        const post = state.input.postList[ i ];
+        const name = post.replace( /.+\//g, '' );
+        state.posts[ name ] = {
+            content: '',
+            runs: {
+                prev: { ms: 0, count: 0 },
+                next: { ms: 0, count: 0 }
+            },
+            status: 'loading',
+            transfer: {
+                loaded: 0,
+                total: 0
+            }
+        }
+
+        queueNext();
+
+        const xhr = new XMLHttpRequest();
+        xhr.open( 'GET', post );
+
+        xhr.onprogress = event => {
+            state.posts[ name ].transfer = {
+                loaded: event.loaded,
+                total: event.total
+            }
+
+            queueNext();
+        };
+
+        xhr.onload = () => {
+            state.posts[ name ].content = xhr.responseText;
+            state.posts[ name ].status = 'loaded';
+            loadedPosts += 1;
+
+            queueNext();
+        };
+
+        xhr.onerror = () => {
+            state.posts[ name ].transfer.status = xhr.status;
+            state.posts[ name ].status = 'failed';
+            loadedPosts += 1;
+
+            queueNext();
+        };
+
+        xhr.send();
+    }
+}
+
+function compareOutputs( next ) {
+    const startedAt = performance.now();
+    const output = document.getElementById( 'comparing-parsers' );
+    output.innerHTML = '';
+    const o = m => output.innerHTML += m;
+
+    o( '<p>Comparing parsers&hellip;</p>' );
+
+    o( '<ul>' );
+    for ( let name in state.posts ) {
+        const post = state.posts[ name ];
+
+        if ( post.status !== 'loaded' ) {
+            continue;
+        }
+
+        post.prevParse = state.input.prevParser( post.content );
+        post.nextParse = state.input.nextParser( post.content );
+
+        post.isEqual = JSON.stringify( post.prevParse ) === JSON.stringify( post.nextParse );
+
+        o( '<li>' + name + ': ' + ( post.isEqual ? 'match' : 'mismatch' ) + '</li>' );
+    }
+    o( '</ul>' );
+
+    const endedAt = performance.now();
+    o( '<p>Took ' + Math.ceil( endedAt - startedAt ) + ' ms to compare parsers</p>' );
+
+    next();
+}
+
+function benchmark( next ) {
+    const startedAt = performance.now();
+    const output = document.getElementById( 'benchmark' );
+    output.innerHTML = '';
+    const o = m => output.innerHTML += m;
+    let stillRunning = true;
+
+    const stopper = document.createElement( 'button' );
+    stopper.setAttribute( 'id', 'stop-benchmark' );
+    stopper.innerHTML = 'Stop benchmark';
+    stopper.addEventListener( 'click', () => {
+        stillRunning = false;
+        stopper.parentNode.removeChild( stopper );
+    } );
+    output.parentNode.appendChild( stopper );
+
+    const finish = () => {
+        const endedAt = performance.now();
+        o( '<p>Took ' + Math.ceil( endedAt - startedAt ) / 1000 + ' s to run benchmark</p>' );
+        next();
+    };
+
+    o( '<div id="benchmark-results"></div>' );
+
+    const posts = Object.keys( state.posts ).filter( name => state.posts[ name ].status === 'loaded' );
+    const postCount = posts.length;
+
+    const report = document.getElementById( 'benchmark-results' );
+    const ul = document.createElement( 'ul' );
+    for ( let i = 0; i < posts.length; i++ ) {
+        const name = posts[ i ];
+        const post = state.posts[ name ];
+        const id = 'post-report-' + name;
+        const li = document.createElement( 'ui' );
+        li.setAttribute( 'id', id );
+        li.innerHTML = name + ': not yet benchmarked';
+        state.posts[ name ].node = li;
+        ul.appendChild( li );
+    }
+    report.appendChild( ul );
+
+    const loop = () => {
+        // grab a random post and randomly pick the parser
+        const choice = ( Math.random() > 0.5 ) ? 'prev' : 'next';
+        const parse = choice === 'prev' ? state.input.prevParser : state.input.nextParser;
+        const name = posts[ Math.floor( Math.random() * postCount ) ];
+        const post = state.posts[ name ];
+        const tic = performance.now();
+        let toc = tic;
+        let count = 0;
+        let ms = 0;
+
+        do {
+            const content = post.content + '<p>' + count + '</p>';
+            const _tic = performance.now();
+            parse( content );
+            toc = performance.now();
+            count += 1;
+            ms += toc - _tic;
+        } while ( toc - tic < LOOP_TIME );
+
+        post.runs[ choice ].ms += ms;
+        post.runs[ choice ].count += count;
+
+        post.node.innerHTML = (
+            '<div>' + 
+            '<p><strong>' + name + '</strong></p>' +
+            '<p>prev: ' + Math.round( 1000 * post.runs.prev.ms / ( post.runs.prev.count + Number.EPSILON ) ) / 1000 + ' ms avg (' + post.runs.prev.count + ' runs)</p>' +
+            '<p>next: ' + Math.round( 1000 * post.runs.next.ms / ( post.runs.next.count + Number.EPSILON ) ) / 1000 + ' ms avg (' + post.runs.next.count + ' runs)</p>' +
+            '</div>'
+        );
+
+        setTimeout( stillRunning ? loop : finish, 10 );
+    };
+
+    setTimeout( loop, 10 );
+}
+
+function runParser() {
+    (
+        () => initialize( 
+        () => loadPosts( 
+        () => compareOutputs( 
+        () => benchmark( 
+        () => l( 'Done' ) 
+    ) ) ) ) )();
+}
